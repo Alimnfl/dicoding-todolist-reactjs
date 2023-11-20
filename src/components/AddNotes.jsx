f9428e38a4eee715f35ad0000180b2aef6f2cad4
Alimnfl <mnaufallim234@gmail.com>
2023-11-21 06:13:23 +0700

    Updated AddNotes

--- src/components/AddNotes.jsx
+++ src/components/AddNotes.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-function AddTodos({ handleAddTodo }) {
-  const [todoText, setTodoText] = useState({
+function AddTodos({ handleAddNote }) {
+  const [noteText, setNoteText] = useState({
     id: '0',
     title: '',
     body: '',
@@ -13,8 +13,8 @@ function AddTodos({ handleAddTodo }) {
   const characterLimitTitle = 25;
   const handleChangeTitle = (e) => {
     if (e.target.value.trim().length <= characterLimitTitle) {
-      setTodoText({
-        ...todoText,
+      setNoteText({
+        ...noteText,
         title: e.target.value,
       });
     }
@@ -23,26 +23,26 @@ function AddTodos({ handleAddTodo }) {
   const characterLimitBody = 50;
   const handleChangeBody = (e) => {
     if (e.target.value.trim().length <= characterLimitBody) {
-      setTodoText({
-        ...todoText,
+      setNoteText({
+        ...noteText,
         body: e.target.value,
       });
     }
   };
 
   const handleSaveClick = () => {
-    if (todoText.body.trim().length > 0) {
-      handleAddTodo(todoText.title, todoText.body, todoText.archived, todoText.createdAt);
-      setTodoText({ ...todoText, title: '', body: '' });
+    if (noteText.body.trim().length > 0) {
+      handleAddNote(noteText.title, noteText.body, noteText.archived, noteText.createdAt);
+      setNoteText({ ...noteText, title: '', body: '' });
     }
   };
 
   return (
     <div className="flex flex-col drop-shadow-md justify-between mt-6 p-4 w-[400px] border h-[200px] bg-blue-400 rounded-xl">
-      <input className="justify-start w-full h-[20px] m-1 text-lg font-semibold placeholder-gray-600 bg-blue-400" onChange={handleChangeTitle} value={todoText.title} placeholder="Texting this title" />
-      <textarea className="justify-start w-full h-full m-1 text-xs placeholder-gray-600 bg-blue-400" onChange={handleChangeBody} value={todoText.body} placeholder="Texting here your description" />
+      <input className="justify-start w-full h-[20px] m-1 text-lg font-semibold placeholder-gray-600 bg-blue-400" onChange={handleChangeTitle} value={noteText.title} placeholder="Texting this title" />
+      <textarea className="justify-start w-full h-full m-1 text-xs placeholder-gray-600 bg-blue-400" onChange={handleChangeBody} value={noteText.body} placeholder="Texting here your description" />
       <div className="flex flex-row items-center justify-between">
-        <p className="">{characterLimitBody - todoText.body.length} remaining</p>
+        <p className="">{characterLimitBody - noteText.body.length} remaining</p>
         <button className="p-1 px-3 rounded-lg bg-slate-200" onClick={handleSaveClick}>
           Save
         </button>
@@ -52,7 +52,7 @@ function AddTodos({ handleAddTodo }) {
 }
 
 AddTodos.propTypes = {
-  handleAddTodo: PropTypes.func.isRequired,
+  handleAddNote: PropTypes.func.isRequired,
 };
 
 export default AddTodos;
